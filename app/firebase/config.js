ff8c153625c10f33e4d448e3c7847f9d7a0f6ced
PARTH DUDHE <parthdudhe06@gmail.com>
2025-09-14 20:02:18 +0530

    feat:added firestore db  connection

--- app/firebase/config.js
+++ app/firebase/config.js
@@ -1,6 +1,6 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import {getAuth} from 'firebase/auth'
-
+import {getFirestore} from 'firebase/firestore'
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -11,7 +11,7 @@ const firebaseConfig = {
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
-
+const db = getFirestore(app);
 const auth = getAuth(app)
 
-export {app, auth}
+export {app, auth, db}
